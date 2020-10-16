2e5c02a5fe510ca21cad65ae39dd084581603908
Tass Lorine <tlorine@ga-o3.msk.21-school.ru>
2020-10-16 17:29:12 +0300

    mikro-fix

--- server.py
+++ server.py
@@ -4,9 +4,13 @@ from threading import Thread
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 10003
 
+users_list = dict()
+
 print(HOST)
 
-def mailing_proc(conn, user_name):
+def mailing_proc(conn, users_list):
+    user_name = conn.recv(1024).decode()
+    users_list[user_name] = conn
     while True:
         data = conn.recv(1024).decode() #принимает данные от клиента(размер данных)
         print(data)
@@ -15,16 +19,14 @@ def mailing_proc(conn, user_name):
             break
         for key in users_list:
             if key != user_name:
-                users_list[key].send(f"{user_name}:\t{data}".encode())
+                users_list[key].send(f"{user_name}:\t{data}\n".encode())
 
-users_list = dict()
 sock = socket.socket() # создания сокета
 sock.bind((HOST, PORT)) #связать с хостом и портом 
 sock.listen(2) #cокет теперь слушает подлючения (принимает 1 аргумент - сколько подклчений возможно за раз)
 while True:
     conn, addr = sock.accept() #ждет клиента. возращает новый сокет и номер клиента
-    user_name = conn.send("Enter Login:".encode())
-    users_list[user_name] = conn
-    mailing = Thread(target=mailing_proc, args=(conn, user_name))
+    conn.send("Enter Login:".encode())
+    mailing = Thread(target=mailing_proc, args=(conn, users_list))
     mailing.start()
 sock.close()
